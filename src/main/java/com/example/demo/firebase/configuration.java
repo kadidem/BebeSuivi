44873229e0da1ac2782eaec565f8f0fc1552f891
kadidia mah Dembele <kadidem1999@gmail.com>
2023-11-01 15:40:16 +0000

    premier commit

---
+++ src/main/java/com/example/demo/firebase/configuration.java
@@ -0,0 +1,28 @@
+package com.example.demo.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+@Service
+public class configuration {
+@PostConstruct
+    public void initialisation ()throws IOException{
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("./serviceAccount.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
